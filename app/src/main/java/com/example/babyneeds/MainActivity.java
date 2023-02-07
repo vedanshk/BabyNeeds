09fc105453d0956d9ac847546c6b1b2a254cce27
vedansh kumar <56169665+vedanshk@users.noreply.github.com>
2023-02-07 21:16:23 +0530

    -

--- app/src/main/java/com/example/babyneeds/MainActivity.java
+++ app/src/main/java/com/example/babyneeds/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.babyneeds;
 
 import android.os.Bundle;
 
+import com.example.babyneeds.data.DatabaseHandler;
+import com.example.babyneeds.model.Item;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -29,11 +31,15 @@ public class MainActivity extends AppCompatActivity {
     private AlertDialog dialog;
     private Button saveButton;
     private EditText babyItem , itemQuantity  , itemColor , itemSize;
+
+    private DatabaseHandler databaseHandler;
   FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHandler = new DatabaseHandler(this);
 
         fab = findViewById(R.id.fab);
 
@@ -44,22 +50,59 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
+        
 
 
     }
+
+    private void saveItem(View v) {
+        Item item  = new Item();
+        String newItem = babyItem.getText().toString().trim();
+        String newColor = itemColor.getText().toString().trim();
+
+        int quantity = Integer.parseInt(itemQuantity.getText().toString().trim());
+
+        int size = Integer.parseInt(itemSize.getText().toString().trim());
+
+        item.setItemName(newItem);
+        item.setQuantity(quantity);
+        item.setColor(newColor);
+        item.setSize(size);
+        databaseHandler.addItem(item);
+
+        Snackbar.make(v , "Item Saved" , Snackbar.LENGTH_SHORT).show();
+
+    }
+
 
     private void createPopupItem() {
 
         builder = new AlertDialog.Builder(this);
         View view = getLayoutInflater().inflate(R.layout.popup , null);
 
+
         babyItem = view.findViewById(R.id.edtTxtItem);
         itemQuantity = view.findViewById(R.id.edtTxtQty);
         itemColor  = view.findViewById(R.id.edtTxtColor);
         itemSize = view.findViewById(R.id.edtTxtSize);
+        saveButton = view.findViewById(R.id.btnSave);
         builder.setView(view);
         dialog = builder.create(); // creating dialog
         dialog.show(); // showing dialog
+
+        saveButton.setOnClickListener(v->{
+
+            if(!babyItem.getText().toString().isEmpty() &&!itemSize.getText().toString().isEmpty()
+                    &&!itemColor.getText().toString().isEmpty()&& !itemQuantity.getText().toString().isEmpty()
+            ){
+                saveItem(v);
+
+            }else{
+                Snackbar.make(v , "Empty Fields not allowed" ,Snackbar.LENGTH_SHORT).show();
+            }
+        });
+
+
 
     }
 
